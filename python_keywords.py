14c1d7cee3962ff8d02ecb02d4d4e32c2160613e
Harry Huang <hy.harry.huang@gmail.com>
2015-02-11 17:17:35 +0000

    Initial commit

---
+++ python_keywords.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# coding: utf8
+from nltk.corpus import stopwords
+import re
+import string
+
+cachedStopWords = stopwords.words("english")
+
+def removeStopWordsFromString(string):
+    return ' '.join([word for word in string.split() if word.lower() not in cachedStopWords])
+
+def removeLinksFromString(string):
+	return re.sub(r"(?:\@|https?\://)\S+", "", string).strip()
+
+def removeHashtagsFromString(string):
+	return re.sub("#", "", string).strip()
+
+#remove retweets
+def removeRTsFromString(string):
+	return re.sub(r"(?i)\brt\b", "", string).strip()
+
+def removePunctuationFromString(string):
+	for p in string.punctuation:
+		string = string.replace(p, "")
+
+	return string.strip()
